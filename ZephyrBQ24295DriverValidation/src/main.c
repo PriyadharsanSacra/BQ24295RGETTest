7b60301d673a37647b9b54e2db3cda4542c47341
Priyadharsan <priyadharsan@sacrasys.com>
2026-07-10 16:20:25 +0530

    Added functionality to test the charger online property in main.

--- ZephyrBQ24295DriverValidation/src/main.c
+++ ZephyrBQ24295DriverValidation/src/main.c
@@ -1,15 +1,24 @@
+/*
+ * Copyright 2026 Sacra Systems Private Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
+#include <zephyr/drivers/charger.h>
 
 #define BQ24295_NODE DT_NODELABEL(bq24295)
 
 int main(void)
 {
-	const struct device *dev = DEVICE_DT_GET(BQ24295_NODE);
+	const struct device *charger = DEVICE_DT_GET(BQ24295_NODE);
+	union charger_propval val;
+	int ret;
 
-	printk("\nBQ24295 Driver Validation\n");
+	printk("\n=== BQ24295 Charger Driver Validation ===\n");
 
-	if (!device_is_ready(dev)) {
+	if (!device_is_ready(charger)) {
 		printk("BQ24295 device not ready\n");
 		return 0;
 	}
@@ -17,6 +26,22 @@ int main(void)
 	printk("BQ24295 device ready\n");
 
 	while (1) {
-		k_sleep(K_SECONDS(5));
+
+		ret = charger_get_prop(charger,
+				       CHARGER_PROP_ONLINE,
+				       &val);
+
+		if (ret == 0) {
+			printk("ONLINE : %s\n",
+			       val.online == CHARGER_ONLINE_FIXED ? "YES" : "NO");
+		} else if (ret == -ENOTSUP) {
+			printk("ONLINE property not supported\n");
+		} else {
+			printk("charger_get_prop() failed (%d)\n", ret);
+		}
+
+		k_sleep(K_SECONDS(2));
 	}
+
+	return 0;
 }
